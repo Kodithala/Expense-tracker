469224f86d2593cf140c9c796e59d0bfc72cf5bc
Kodithala <anudeepikakodithala724@gmail.com>
2026-09-09 20:00:31 +0530

    Hook admin superuser creation to post_migrate signal

--- expenses/apps.py
+++ expenses/apps.py
@@ -1,6 +1,36 @@
+import os
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_superuser(sender, **kwargs):
+    try:
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Admin12345!')
+
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={'email': email, 'is_staff': True, 'is_superuser': True}
+        )
+        user.email = email
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
+        if created:
+            print(f'Superuser "{username}" created successfully.')
+        else:
+            print(f'Superuser "{username}" password updated.')
+    except Exception as e:
+        print(f'Superuser creation note: {e}')
 
 class ExpensesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'expenses'
+
+    def ready(self):
+        post_migrate.connect(create_default_superuser, sender=self)
+
+
